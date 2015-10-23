472b8726eeb3700f0ce49efbd23c111ba6e8679e
Mariano Flores <mariano.flores@xero.com>
2015-10-23 16:43:30 +1300

    Compare properties of each step with the search query parameter.

--- lib/BuildType.rb
+++ lib/BuildType.rb
@@ -1,16 +1,21 @@
 module TeamcityPrisma
   
   class BuildType
+    
     Thread.abort_on_exception=true
     @@params = ["-t", "build_type"]
     $result = Array.new
     @@elements = Array.new
     @@counter = 0
+    @@contador = 0
+    @@found = 0
+    @@steps = 0
     
-    def find_string(site, string)
+    def find_string(site, string, step_type)
       @string = string
+      @step_type = step_type
       #TeamcityPrisma::Core.new(@@params + ["-S", "#{site}", "-s", "#{seconds}", "-o", "#{operator}", "-m", "search"])
-      TeamcityPrisma::Core.new(@@params + ["-S", "#{site}", "-s", "#{string}", "-o", "contains", "-m", "search"])
+      TeamcityPrisma::Core.new(@@params + ["-S", "#{site}", "-s", "#{string}", "-o", "contains", "-m", "search", "-z", "#{step_type}"])
     end
     
     
@@ -21,14 +26,15 @@ module TeamcityPrisma
       
       puts build_types.count*0.002
       @@blocks_total = 10.0
+      @@blocks_total = build_types.size if build_types.size < 5
       blocks = Array.new
       blocks = build_types.each_slice( (build_types.count/@@blocks_total).round ).to_a
       
-      
-     
+      puts build_types.count
+      puts blocks.count
       threads = []
       blocks.each do |block|
-        print block.count.pretty_inspect()
+        
         threads << Thread.new do
                      _get_steps_ignore_templates(block)
                    end                           
@@ -38,12 +44,40 @@ module TeamcityPrisma
       #_get_steps_ignore_templates(build_types)
       
       #puts @@elements.pretty_inspect
+      puts "Threads: #{@@blocks_total}"
       puts "Builds in total: #{build_types.count}"
-      puts "Builds to analyze: #{@@elements.count} "  
+      puts "Steps to analyze: #{@@steps} "  
       
+   
+        #$result = @@elements
+        #$result.flatten!()
       
-      $result = @@elements
-      $result.flatten!()
+#=begin
+      
+        
+        @@blocks_total = @@elements.size if @@elements.size < 5
+        threads = []
+        blocks = Array.new
+        blocks = @@elements.each_slice( (@@elements.size/@@blocks_total).round ).to_a
+           
+
+        blocks.each do |block|        
+          threads << Thread.new do
+                     _get_properties(block)  
+                   end               
+        end  
+            
+        ThreadsWait.all_waits(threads)
+              
+        
+   
+      
+      puts "\n\nTotal found: #{@@found}"      
+      $result.flatten!()            
+    
+        
+#=end      
+      
       
       #blocks = Array.new
       #blocks = @@elements.each_slice( (@@elements.size/@@blocks_total).round ).to_a
@@ -71,7 +105,10 @@ module TeamcityPrisma
       #puts ">>>>>>>>>>>> total builds #{total}"
         
       builds.each do |build_type|
+
+#=begin
         
+                
         #puts ">>>>>>>>>>>> #{build_type.id}" 
         
         #cli_percentage(@@counter, @@blocks_total, "template?", build_type.id)
@@ -81,56 +118,79 @@ module TeamcityPrisma
               print "#{@@counter*100/@@blocks_total.round/total}%#{9.chr}template?: #{build_type.id} \r"
         
         
-       
+  
+   
         #While the script is running a build could be deleted. Prevent this situation.
         unless TeamCity.buildtype(id: "#{build_type.id}").nil?      
        
-          
+
+                    
           #Collect all the steps of builds that are not based on Templates.             
           if TeamCity.buildtype(id: "#{build_type.id}")["template"].nil?
-            
+
+
+                        
             steps = Array.new
+ 
+ 
             
+                       
             unless TeamCity.buildtype(id: "#{build_type.id}")["steps"]["step"].nil?
+              counter = 0
               TeamCity.buildtype(id: "#{build_type.id}")["steps"]["step"].each do |step|
-                steps << step.id
+                steps << counter
+                counter = counter + 1
               end
             end
             
+            #$files.write_items({ id: "#{build_type.id}", steps: steps }) if steps.count > 0
             @@elements << { id: "#{build_type.id}", steps: steps } if steps.count > 0
+            @@steps = @@steps + steps.count
           
               
         
             print "#{build_type.id}" + fill + "\n"
               
-            
+             
+      
+      
           #For builds based on templates ignore their inherited steps.  
           else
             
             steps_template = Array.new
             
             unless TeamCity.buildtype_template(id: build_type.id)["steps"]["step"].nil?
+              
               TeamCity.buildtype_template(id: build_type.id)["steps"]["step"].each do |step|
                 steps_template << step.id
+                
               end
             end
             
             steps = Array.new
             
+            counter = 0
             TeamCity.buildtype(id: "#{build_type.id}")["steps"]["step"].each do |step|
               if !steps_template.include?(step.id)
-                steps << step.id
+                steps << counter
+                counter = counter + 1
               end
             end
             
                       
+            #$files.write_items( { id: "#{build_type.id}", steps: steps }) if steps.count > 0
+            
             @@elements << { id: "#{build_type.id}", steps: steps } if steps.count > 0
-            
-            
+            @@steps = @@steps + steps.count
+           
+                       
           end
         
         end
+                
         @@counter = @@counter + 1
+        
+#=end
       end
       
       150.times{print " "}
@@ -142,6 +202,96 @@ module TeamcityPrisma
     
     
     
+    
+    
+    
+    
+  def _get_properties(elements)
+             
+       fill = ""
+       65.times do fill = fill + " " end
+       
+       elements.each() do |element|
+         
+         #puts element[:steps].pretty_inspect()
+                  
+         element[:steps].each do |step|
+         
+           #puts step
+          
+           #puts "xxx #{TeamCity.buildtype(id: element[:id])["steps"]["step"][step]["properties"]} \n\n\n"
+           
+           TeamCity.buildtype(id: element[:id])["steps"]["step"][step]["properties"]["property"].each do |property| 
+           
+           total = @@steps
+
+            #puts "stp: #{step} : comparacion #{property["value"].pretty_inspect()} - #{@string}"
+             
+             #puts "#{@@contador}*100 / #{@@blocks_total} / #{total} -  \n\n\n\n"
+         
+             print "#{@@contador} of #{@@steps} steps: "
+             
+             90.times do print " " end
+             print "\r"
+             print "#{@@contador} of #{@@steps} steps, property: #{property.name} \r"
+             
+            
+             
+             if property.value.include?(@string)
+               
+               $result << { id: "#{element[:id]}", step: step, property: property } 
+               print "#{element[:id]} -  #{property.name} " + fill + "\n"
+               
+               @@found = @@found + 1
+               
+             end
+             
+             
+                     
+           
+           end
+         @@contador = @@contador + 1  
+         
+         end
+       
+       
+       end 
+       
+       
+       
+       80.times do print " " end 
+       print "\r"
+
+ 
+   end
+    
+    
+    
+    
+    
+ 
+    
+    
+    
+    
+  def compare(x,y)
+    case @operator
+    when 'minor_than'
+      x.to_i < y.to_i
+      
+    when 'greater_than'
+      x.to_i > y.to_i
+      
+    when 'equals'
+      
+      x.to_i == y.to_i
+      
+    when 'contains'
+      x.to_s.include?(y.to_s)
+    else
+    
+    end
+  end  
     
     
     def cli_percentage(counter, blocks_total, message, message1)
