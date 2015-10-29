539dc0bd5777d2d6905330c8b4463c12357bc53a
Mariano Flores <mariano.flores@xero.com>
2015-10-30 09:59:55 +1300

    Adding replace for Build Types.

--- lib/test.rb
+++ lib/test.rb
@@ -19,8 +19,8 @@ time1 = Time.new
 ###
 #############################################################################################################
 
-$prisma = TeamcityPrisma::VCS.new
-$prisma.find_custom_period("tc1", "501", "minor_than")
+#$prisma = TeamcityPrisma::VCS.new
+#$prisma.find_custom_period("dev", "3601", "minor_than")
 
 
 
@@ -28,19 +28,39 @@ $prisma.find_custom_period("tc1", "501", "minor_than")
 
 #############################################################################################################
 ###
-###     BUILD STEP
+###     BUILD STEP        ----------       S  E  A  R  C  H      ----------
 ###
 #############################################################################################################
 
 #$prisma = TeamcityPrisma::BuildType.new
-#$prisma.find_string("dev","test")
+
+####$prisma.find_string("tc2","test") #  search in all the build steps
+#####$prisma.find_string("tc2","test", "script.content")
+
+#$prisma.find_string("dev","bundle install") #  search in all the build steps
+
+
+
+#############################################################################################################
+###
+###     BUILD STEP     ----------       R  E  P  L  A  C  E      ----------
+###
+#############################################################################################################
+
+$prisma = TeamcityPrisma::BuildType.new
+$prisma.replace_string("tc2","bundle install", "bundle install") #  search in all the build steps
 
 
 
 
+#############################################################################################################
+###
+#############################################################################################################
 
-puts $result.pretty_inspect()
+#puts $result.pretty_inspect()
+puts "#########################################################################\n\n"
 time2 = Time.new
 puts "Process time: "
 puts Time.at(time2-time1).utc.strftime("%H:%M:%S")
+puts "$result.count: #{$result.count}"
 
