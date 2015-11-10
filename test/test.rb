ac27d21da1abe21091e502226ad53104c9f9d76e
Mariano Flores <mariano.flores@xero.com>
2015-11-10 17:21:48 +1300

    TESTS - Including Code Coverage functionality, and all the tests for this framework. Also cleaning the code.

--- test/test.rb
+++ test/test.rb
@@ -1,11 +1,18 @@
+gem 'simplecov', :require => false, :group => :test
+require 'simplecov'
+SimpleCov.start
 gem "minitest"  
 require 'minitest/autorun'
 require 'teamcity-prisma'
+#require 'C:/teamcity-prisma/lib/teamcity-prisma.rb'
 require 'yaml'
 
 class PrismaTest < Minitest::Test
-  
-
+  def setup
+    File.delete('C:\Teamcity-Prisma\result_vcs_search_minor_than_60.yml') if File.exist?('C:\Teamcity-Prisma\result_vcs_search_minor_than_60.yml')
+    File.delete('C:\Teamcity-Prisma\result_buildstep_search.yml') if File.exist?(':\Teamcity-Prisma\result_buildstep_search.yml')
+    File.delete('C:\Teamcity-Prisma\result_buildstep_search.yml') if File.exist?(':\Teamcity-Prisma\result_buildstep_replace.yml')
+  end
   
   #############################################################################################################
   ###
@@ -13,21 +20,11 @@ class PrismaTest < Minitest::Test
   ###
   #############################################################################################################
   def test_vcs_search_minor_than_60
-    
-    
-    result = TeamcityPrisma::Core.new(["vcsroot_find", 'C:/Teamcity-Prisma/secret_constants.rb', "dev", "60", "minor_than", 'C:\Teamcity-Prisma\result_vcs_search_minor_than_60.yml'])
-      
+    result = TeamcityPrisma::Core.new(['vcsroot_find', 'C:/Teamcity-Prisma/secret_constants.rb', 'dev', '60', 'minor_than', 'C:\Teamcity-Prisma\result_vcs_search_minor_than_60.yml'])   
     result = YAML::load_file('C:\Teamcity-Prisma\result_vcs_search_minor_than_60.yml')
-      
-    #testbed = [{"id" => "PackageStoreAPI_Features", "href" => "/httpAuth/app/rest/vcs-roots/id:PackageStoreAPI_Features", "name" => "development"}]
-    
-    testbed = YAML::load_file(File.join(File.dirname(__FILE__), 'test_vcs_search_minor_than_60.yml'))    
-
-    assert_equal result, testbed
-    
-
+    bed = YAML::load_file(File.join(File.dirname(__FILE__), 'bed_vcs_search_minor_than_60.yml'))    
+    assert_equal result, bed  
   end
-  
   
   #############################################################################################################
   ###
@@ -36,20 +33,11 @@ class PrismaTest < Minitest::Test
   #############################################################################################################
 
   def test_buildstep_search
-
-    
-    result = TeamcityPrisma::Core.new(["property_search", 'C:/Teamcity-Prisma/secret_constants.rb', "dev", "installl", 'C:\Teamcity-Prisma\result_buildstep_search.yml'])
-    
+    result = TeamcityPrisma::Core.new(['property_search', 'C:/Teamcity-Prisma/secret_constants.rb', 'dev', 'iinstall', 'C:\Teamcity-Prisma\result_buildstep_search.yml'])
     result = YAML::load_file('C:\Teamcity-Prisma\result_buildstep_search.yml')
-    
-    #testbed = YAML::load_file(File.join(File.dirname(__FILE__), 'test_vcs_search_minor_than_60.yml'))
-      
-    #assert_equal result, testbed
-
-    
+    bed = YAML::load_file(File.join(File.dirname(__FILE__), 'bed_buildstep_search.yml'))
+    assert_equal result, bed
   end
-  
-  
   
   #############################################################################################################
   ###
@@ -58,9 +46,12 @@ class PrismaTest < Minitest::Test
   ############################################################################################################# 
   
   def test_buildstep_replace
-    #TeamcityPrisma::Core.new(["property_replace", 'C:/Teamcity-Prisma/secret_constants.rb',"dev","installl", "install", "result_prisma.txt"])
+    TeamcityPrisma::Core.new(['property_replace', 'C:/Teamcity-Prisma/secret_constants.rb', 'dev', 'test_buildstep_replace_expected', 'test_buildstep_replace_modified', 'C:\Teamcity-Prisma\result_buildstep_replace.yml'])
+    result = TeamcityPrisma::Core.new(['property_replace', 'C:/Teamcity-Prisma/secret_constants.rb','dev', 'test_buildstep_replace_modified', 'test_buildstep_replace_expected', 'C:\Teamcity-Prisma\result_buildstep_replace.yml'])
+    result = YAML::load_file('C:\Teamcity-Prisma\result_buildstep_replace.yml')
+    bed = YAML::load_file(File.join(File.dirname(__FILE__), 'bed_buildstep_replace.yml'))
+    assert_equal result, bed
   end
-  
   
   #############################################################################################################
   ###
@@ -69,25 +60,10 @@ class PrismaTest < Minitest::Test
   #############################################################################################################
   
   def test_buildstep_modify
-    #TeamcityPrisma::Core.new(["property_modify", 'C:/Teamcity-Prisma/secret_constants.rb', "dev","STDIN", "e", "result_prisma.txt"])
+    TeamcityPrisma::Core.new(['property_modify', 'C:/Teamcity-Prisma/secret_constants.rb', 'dev','PS1', 'p', 'C:\Teamcity-Prisma\result_buildstep_modify.yml', nil, 'PackageStoreAPI_TestTemplate'])
+    result = TeamcityPrisma::Core.new(['property_modify', 'C:/Teamcity-Prisma/secret_constants.rb', 'dev','STDIN', 'e', 'C:\Teamcity-Prisma\result_buildstep_modify.yml', nil, 'PackageStoreAPI_TestTemplate'])
+    result = YAML::load_file('C:\Teamcity-Prisma\result_buildstep_modify.yml')
+    bed = YAML::load_file(File.join(File.dirname(__FILE__), 'bed_buildstep_modify.yml'))
+    assert_equal result, bed 
   end
-  
-  
 end
-
-
-=begin
-
-#############################################################################################################
-###
-#############################################################################################################
-
-#puts $result.pretty_inspect()
-puts "#########################################################################\n\n"
-time2 = Time.new
-puts "Process time: "
-puts Time.at(time2-time1).utc.strftime("%H:%M:%S")
-puts "$result.count: #{$result.count}"
-
-
-=end
