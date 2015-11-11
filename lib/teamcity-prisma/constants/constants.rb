eec3f4da646a3950d193808afbc58f524de311fc
Mariano Flores <mariano.flores@xero.com>
2015-11-11 16:39:59 +1300

    REFACTORING - Removing unnecessary constants.

--- lib/teamcity-prisma/constants/constants.rb
+++ lib/teamcity-prisma/constants/constants.rb
@@ -1,5 +1,4 @@
 module TeamcityPrisma
-
   
   ###############################################################################################
   ##
@@ -7,30 +6,17 @@ module TeamcityPrisma
   ##
   ###############################################################################################
   
-  SITE_DEV = "dev"
-  SITE_TC1 = "tc1"
-  SITE_TC2 = "tc2"
-  SITE_TC3 = "tc3"
+  SITE_DEV = 'dev'
+  SITE_TC1 = 'tc1'
+  SITE_TC2 = 'tc2'
+  SITE_TC3 = 'tc3'
   
-  FILE_FOUND = "found.txt"
-  FILE_REPLACE = "replace.txt"
-  FILE_PROCESSED = "processed.txt"
-  FILE_ITEMS = "items.txt"
-  FILE_ITEMS_PROCESSED = "items_processed.txt"
-  
-  PROPERTY_VALUE_SHOW_PS1 = "Execute .ps1 from external file"
-  PROPERTY_VALUE_SHOW_STDIN = "Put script into PowerShell stdin"
-  
-  PROPERTY_VALUE_STDIN = "STDIN"
-  PROPERTY_VALUE_PS1 = "PS1"
-  
-  STEP_TYPE_POWERSHELL = "jetbrains_powershell"
-  PROPERTY_NAME_POWERSHELL_EXECUTION = "jetbrains_powershell_execution"
-  HTML_ELEMENT_VALUE_POWERSHELL_EXECUTION_MODE = "powershell_execution_mode"
   REST_LOCATION = '/httpAuth/app/rest'
   
-  STEP_TYPE_COMMAND_LINE = "simpleRunner"
-  PROPERTY_NAME_COMMAND_LINE_EXECUTION = "jetbrains_powershell_execution"
-  PROPERTY_NAME_SCRIPT_CONTENT = "script.content"
+  FILE_FOUND = 'found.txt'
+  FILE_REPLACE = 'replace.txt'
+  FILE_PROCESSED = 'processed.txt'
+  FILE_ITEMS = 'items.txt'
+  FILE_ITEMS_PROCESSED = 'items_processed.txt'
 
 end
