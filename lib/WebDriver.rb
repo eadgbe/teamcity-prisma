afe1e6a33c75244a0d342bf91c0f37434209abcc
Mariano Flores <mariano.flores@xero.com>
2015-11-12 11:08:59 +1300

    Removing attributes template files, those will be indicated in the README.md file. Removing WebDriver.rb, it was implemented in SeleniumDriver.rb

--- lib/WebDriver.rb
+++
@@ -1,4 +0,0 @@
-@@client = Selenium::WebDriver::Remote::Http::Default.new
-@@client.timeout = 240 # seconds
-@@driver = Selenium::WebDriver.for(:firefox, :http_client => @@client)
-@@driver.manage.timeouts.implicit_wait = 60 # seconds
