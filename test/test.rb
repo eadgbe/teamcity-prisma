ff86622fce724cb0adeb134c018640b3676694f1
Mariano Flores <mariano.flores@xero.com>
2015-11-13 15:52:22 +1300

    REFACTORING - Removing hard coded location for bed test files.

--- test/test.rb
+++ test/test.rb
@@ -9,9 +9,9 @@ require 'yaml'
 
 class PrismaTest < Minitest::Test
   def setup
-    File.delete('C:\Teamcity-Ruby\result_vcs_search_minor_than_60.yml') if File.exist?('C:\Teamcity-Ruby\result_vcs_search_minor_than_60.yml')
-    File.delete('C:\Teamcity-Ruby\result_buildstep_search.yml') if File.exist?(':\Teamcity-Ruby\result_buildstep_search.yml')
-    File.delete('C:\Teamcity-Ruby\result_buildstep_search.yml') if File.exist?(':\Teamcity-Ruby\result_buildstep_replace.yml')
+    File.delete('.\result_vcs_search_minor_than_60.yml') if File.exist?('.\result_vcs_search_minor_than_60.yml')
+    File.delete('.\result_buildstep_search.yml') if File.exist?(':\Teamcity-Ruby\result_buildstep_search.yml')
+    File.delete('.\result_buildstep_search.yml') if File.exist?(':\Teamcity-Ruby\result_buildstep_replace.yml')
   end
   
   #############################################################################################################
@@ -20,8 +20,8 @@ class PrismaTest < Minitest::Test
   ###
   #############################################################################################################
   def test_vcs_search_minor_than_60
-    result = TeamcityRuby::Core.new(['vcsroot_find', 'C:/Teamcity-Ruby/config.rb', 'dev', '60', 'minor_than', 'C:\Teamcity-Ruby\result_vcs_search_minor_than_60.yml'])   
-    result = YAML::load_file('C:\Teamcity-Ruby\result_vcs_search_minor_than_60.yml')
+    result = TeamcityRuby::Core.new(['vcsroot_find', 'C:/Teamcity-Ruby/config.rb', 'dev', '60', 'minor_than', '.\result_vcs_search_minor_than_60.yml'])   
+    result = YAML::load_file('.\result_vcs_search_minor_than_60.yml')
     bed = YAML::load_file(File.join(File.dirname(__FILE__), 'bed_vcs_search_minor_than_60.yml'))    
     assert_equal result, bed  
   end
@@ -33,8 +33,8 @@ class PrismaTest < Minitest::Test
   #############################################################################################################
 
   def test_buildstep_search
-    result = TeamcityRuby::Core.new(['property_search', 'C:/Teamcity-Ruby/config', 'dev', 'iinstall', 'C:\Teamcity-Ruby\result_buildstep_search.yml'])
-    result = YAML::load_file('C:\Teamcity-Ruby\result_buildstep_search.yml')
+    result = TeamcityRuby::Core.new(['property_search', 'C:/Teamcity-Ruby/config', 'dev', 'iinstall', '.\result_buildstep_search.yml'])
+    result = YAML::load_file('.\result_buildstep_search.yml')
     bed = YAML::load_file(File.join(File.dirname(__FILE__), 'bed_buildstep_search.yml'))
     assert_equal result, bed
   end
@@ -46,9 +46,9 @@ class PrismaTest < Minitest::Test
   ############################################################################################################# 
   
   def test_buildstep_replace
-    TeamcityRuby::Core.new(['property_replace', 'C:/Teamcity-Ruby/config.rb', 'dev', 'test_buildstep_replace_expected', 'test_buildstep_replace_modified', 'C:\Teamcity-Ruby\result_buildstep_replace.yml'])
-    result = TeamcityRuby::Core.new(['property_replace', 'C:/Teamcity-Ruby/config.rb','dev', 'test_buildstep_replace_modified', 'test_buildstep_replace_expected', 'C:\Teamcity-Ruby\result_buildstep_replace.yml'])
-    result = YAML::load_file('C:\Teamcity-Ruby\result_buildstep_replace.yml')
+    TeamcityRuby::Core.new(['property_replace', 'C:/Teamcity-Ruby/config.rb', 'dev', 'test_buildstep_replace_expected', 'test_buildstep_replace_modified', '.\result_buildstep_replace.yml'])
+    result = TeamcityRuby::Core.new(['property_replace', 'C:/Teamcity-Ruby/config.rb','dev', 'test_buildstep_replace_modified', 'test_buildstep_replace_expected', '.\result_buildstep_replace.yml'])
+    result = YAML::load_file('.\result_buildstep_replace.yml')
     bed = YAML::load_file(File.join(File.dirname(__FILE__), 'bed_buildstep_replace.yml'))
     assert_equal result, bed
   end
@@ -60,9 +60,9 @@ class PrismaTest < Minitest::Test
   #############################################################################################################
   
   def test_buildstep_modify
-    TeamcityRuby::Core.new(['property_modify', 'C:/Teamcity-Ruby/config.rb', 'dev','PS1', 'p', 'C:\Teamcity-Ruby\result_buildstep_modify.yml', nil, 'PackageStoreAPI_TestTemplate'])
-    result = TeamcityRuby::Core.new(['property_modify', 'C:/Teamcity-Ruby/config.rb', 'dev','STDIN', 'e', 'C:\Teamcity-Ruby\result_buildstep_modify.yml', nil, 'PackageStoreAPI_TestTemplate'])
-    result = YAML::load_file('C:\Teamcity-Ruby\result_buildstep_modify.yml')
+    TeamcityRuby::Core.new(['property_modify', 'C:/Teamcity-Ruby/config.rb', 'dev','PS1', 'p', '.\result_buildstep_modify.yml', nil, 'PackageStoreAPI_TestTemplate'])
+    result = TeamcityRuby::Core.new(['property_modify', 'C:/Teamcity-Ruby/config.rb', 'dev','STDIN', 'e', '.\result_buildstep_modify.yml', nil, 'PackageStoreAPI_TestTemplate'])
+    result = YAML::load_file('.\result_buildstep_modify.yml')
     bed = YAML::load_file(File.join(File.dirname(__FILE__), 'bed_buildstep_modify.yml'))
     assert_equal result, bed 
   end
