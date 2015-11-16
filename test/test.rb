d8c4e79d8a80d42647ede8eb59287337c4995d42
Mariano Flores <mariano.flores@xero.com>
2015-11-16 15:20:50 +1300

    DEVOPS-1822 - Adding search for VCS roots minor than 500 seconds for TC2

--- test/test.rb
+++ test/test.rb
@@ -27,8 +27,10 @@ class PrismaTest < Minitest::Test
   end
   
   #TC1
+  #TC2
   def test_vcs_search_minor_than_500
-    result = TeamcityRuby::Core.new(['vcsroot_find', '.\config.rb', 'tc1', '500', 'minor_than', '.\result_vcs_search_minor_than_500_tc1.yml'])   
+    result = TeamcityRuby::Core.new(['vcsroot_find', '.\config.rb', 'tc1', '500', 'minor_than', '.\result_vcs_search_minor_than_500_tc1.yml'])
+    result = TeamcityRuby::Core.new(['vcsroot_find', '.\config.rb', 'tc2', '500', 'minor_than', '.\result_vcs_search_minor_than_500_tc1.yml'])     
   end
   
   #############################################################################################################
