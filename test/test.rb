49a4798f4e49ac232b1f7abbba62c4b74c2b793f
Mariano Flores <mariano.flores@xero.com>
2015-11-24 14:26:26 +1300

    DEVOPS-1822 - Test Datadog in TC1

--- test/test.rb
+++ test/test.rb
@@ -38,6 +38,13 @@ class PrismaTest < Minitest::Test
   #TC2
   def test_vcs_search_minor_than_500
     result = TeamcityRuby::Core.new(['vcsroot_find', '.\config.rb', 'tc1', '500', 'minor_than', '.\result_vcs_search_minor_than_500_tc1.yml'])
+    #Datagod Gauge
+    if result.size() > 0
+      statsd = Statsd.new
+      statsd.gauge('tc1 VCS roots minor than 500', result.count)
+      puts "result.count : #{result.count}"
+    end
+    
     result = TeamcityRuby::Core.new(['vcsroot_find', '.\config.rb', 'tc2', '500', 'minor_than', '.\result_vcs_search_minor_than_500_tc2.yml'])     
   end
   
